151322cd0a87024879d5f46ed96202983473511d
Adriano Ferreira <a.r.ferreira@gmail.com>
2017-08-06 14:47:37 -0700

    Add docs to Kevin::Command::kevin

--- lib/Kevin/Command/kevin.pm
+++ lib/Kevin/Command/kevin.pm
@@ -10,8 +10,73 @@ See 'APPLICATION kevin help COMMAND' for more information on a specific
 command.
 EOF
 has message    => sub { shift->extract_usage . "\nCommands:\n" };
-has namespaces => sub { ['Kevin::Command::kevin', 'Minion::Command::minion'] };
+has namespaces => sub { ['Kevin::Command::kevin'] };
 
 sub help { shift->run(@_) }
 
 1;
+
+=encoding utf8
+
+=head1 NAME
+
+Kevin::Command::kevin - Alternative Minion command
+
+=head1 SYNOPSIS
+
+  Usage: APPLICATION kevin COMMAND [OPTIONS]
+
+=head1 DESCRIPTION
+
+L<Kevin::Command::kevin> lists available alternative L<Minion> commands.
+
+=head1 ATTRIBUTES
+
+L<Kevin::Command::kevin> inherits all attributes from
+L<Mojolicious::Commands> and implements the following new ones.
+
+=head2 description
+
+  my $description = $cmd->description;
+  $cmd            = $cmd->description('Foo');
+
+Short description of this command, used for the command list.
+
+=head2 hint
+
+  my $hint = $cmd->hint;
+  $cmd     = $cmd->hint('Foo');
+
+Short hint shown after listing available L<Minion> commands.
+
+=head2 message
+
+  my $msg = $cmd->message;
+  $cmd    = $cmd->message('Bar');
+
+Short usage message shown before listing available L<Minion> commands.
+
+=head2 namespaces
+
+  my $namespaces = $cmd->namespaces;
+  $cmd           = $cmd->namespaces(['MyApp::Command::kevin']);
+
+Namespaces to search for available alternative L<Minion> commands, defaults to
+L<Kevin::Command::kevin>.
+
+=head1 METHODS
+
+L<Kevin::Command::kevin> inherits all methods from L<Mojolicious::Commands>
+and implements the following new ones.
+
+=head2 help
+
+  $minion->help('app');
+
+Print usage information for alternative L<Minion> command.
+
+=head1 SEE ALSO
+
+L<Minion>, L<Mojolicious::Guides>, L<http://mojolicious.org>.
+
+=cut
