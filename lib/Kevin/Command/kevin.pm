87d7627844fa9e3f7c58194ab41d12dcb1f1f8b3
Adriano Ferreira <a.r.ferreira@gmail.com>
2017-08-11 11:41:13 -0700

    kevin: tweak docs

--- lib/Kevin/Command/kevin.pm
+++ lib/Kevin/Command/kevin.pm
@@ -33,29 +33,29 @@ L<Mojolicious::Commands> and implements the following new ones.
 
 =head2 description
 
-  my $description = $cmd->description;
-  $cmd            = $cmd->description('Foo');
+  my $description = $command->description;
+  $command        = $command->description('Foo');
 
 Short description of this command, used for the command list.
 
 =head2 hint
 
-  my $hint = $cmd->hint;
-  $cmd     = $cmd->hint('Foo');
+  my $hint = $command->hint;
+  $command = $command->hint('Foo');
 
 Short hint shown after listing available L<Minion> commands.
 
 =head2 message
 
-  my $msg = $cmd->message;
-  $cmd    = $cmd->message('Bar');
+  my $msg  = $command->message;
+  $command = $command->message('Bar');
 
 Short usage message shown before listing available L<Minion> commands.
 
 =head2 namespaces
 
-  my $namespaces = $cmd->namespaces;
-  $cmd           = $cmd->namespaces(['MyApp::Command::kevin']);
+  my $namespaces = $command->namespaces;
+  $command       = $command->namespaces(['MyApp::Command::kevin']);
 
 Namespaces to search for available alternative L<Minion> commands, defaults to
 L<Kevin::Command::kevin>.
@@ -67,7 +67,7 @@ and implements the following new ones.
 
 =head2 help
 
-  $minion->help('app');
+  $command->help('app');
 
 Print usage information for alternative L<Minion> command.
 
