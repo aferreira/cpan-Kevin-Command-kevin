3e87fcfafb34c4133c25081d78426ee9c4e78c41
Adriano Ferreira <a.r.ferreira@gmail.com>
2017-08-11 11:58:55 -0700

    Deprecate Kevin::Plugin::kevin

--- lib/Kevin/Plugin/kevin.pm
+++ lib/Kevin/Plugin/kevin.pm
@@ -1,47 +1,18 @@
 package Kevin::Plugin::kevin;
 
 # ABSTRACT: Mojolicious plugin for alternative minion commands
-use Mojo::Base 'Mojolicious::Plugin';
-
-sub register {
-  my ($self, $app, $conf) = @_;
-
-  unshift @{$app->commands->namespaces}, 'Kevin::Command';
-}
+use Mojo::Base 'Mojolicious::Plugin::Kevin::Commands';
 
 1;
 
 =encoding utf8
 
-=head1 SYNOPSIS
-
-  # plugin for Minion
-  $self->plugin(Minion => {Pg => 'postgresql://postgres@/test'});
-
-  # then
-  $self->plugin('Kevin::Plugin::kevin');
-
-  # run
-  ./app.pl kevin worker
-
 =head1 DESCRIPTION
 
-This setups the L<kevin worker> command with the alternative
-L<Kevin::Command::kevin::worker>.
-
-=head1 METHODS
-
-L<Kevin::Plugin::kevin> inherits all methods from
-L<Mojolicious::Plugin> and implements the following new ones.
-
-=head2 register
-
-  $plugin->register(Mojolicious->new);
-
-Register plugin in L<Mojolicious> application.
+DEPRECATED! Use L<Mojolicious::Plugin::Kevin::Commands> instead.
 
 =head1 SEE ALSO
 
-L<Minion>, L<Mojolicious::Guides>, L<http://mojolicious.org>.
+L<Mojolicious::Plugin::Kevin::Commands>.
 
 =cut
